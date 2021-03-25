ff03d6824a6c2c731b62b9d03efaaa7a5bf08c34
shawnkost <shawnmkost@gmail.com>
2021-03-25 16:17:36 -0700

    Created functionality for a user to login, and for their token to be automatically refreshed when it's about to expire

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,8 @@
 import React from "react"
+import useAuth from "./useAuth"
 
 export default function Dashboard(props) {
+  const accessToken = useAuth(props.code);
   return (
     <div>
       {props.code}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const cors = require('cors');
 const SpotifyWebApi = require("spotify-web-api-node");
 const spotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.post('/refresh', (req, res) => {
+  console.log("hi");
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "58c63d6c452940378c16d1e4a5a65ee0",
+    clientSecret: "33463e67ddd9490c9c62042eea3e94a0",
+    refreshToken,
+  })
+
+spotifyApi.refreshAccessToken().then(
+  (data) => {
+    res.json({
+      accessToken: data.body.accessToken,
+      expiresIn: data.body.expiresIn
+    })
+  }).catch(() => {
+    res.sendStatus(400);
+  })
+})
 
 app.post('/login', (req, res) => {
   const code = req.body.code;
@@ -22,3 +47,5 @@ app.post('/login', (req, res) => {
     res.sendStatus(400)
   })
 })
+
+app.listen(3001)
